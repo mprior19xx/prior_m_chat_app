af46ce405f0828e88b0a2397566c2b9a59918601
Mike Prior <mprior19xx@gmail.com>
2020-01-28 17:30:41 -0500

    setting up vue

--- public/js/main_vm.js
+++ public/js/main_vm.js
@@ -1,16 +1,49 @@
-// imports always go first - if we're importing anything7
+// imports always go first - if we're importing anything
+import ChatMessage from "./modules/ChatMessage.js";
 
 const socket = io();
 
-function setUserId(packet) {
-    debugger;
-    console.log(packet);
+function setUserId({sID, message}) {
+    //debugger;
+    vm.socketID = sID;
+    
 }
 
 function runDisconnectMessage (packet) {
-    debugger;
+    //debugger;
     console.log(packet);
 }
+
+//this is our main vue instance
+const vm = new Vue({
+    data: {
+        socketID: "",
+        messages: [], 
+        message:"",
+        nickName:""
+    },
+
+    methods: {
+        dispatchMessage(){
+            //emit a msg event and sed the msg to the server
+            console.log('handle send message');
+
+            socket.emit('chat_message', { 
+                content: this.message,
+                name: this.nickName || "anonymous", // || is OR operator, if nickName is blank make anon
+            });
+        },
+
+    },
+
+    components:{
+        newmessage: ChatMessage
+    },
+
+    mounted: function(){
+        console.log('mounted');
+    }
+}).$mount("#app");
 
 
 
