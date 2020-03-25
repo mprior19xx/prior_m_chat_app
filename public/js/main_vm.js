c5172fbd3e086f17652b482331065e2d78e0c3ca
Mike Prior <mprior19xx@gmail.com>
2020-03-25 09:16:07 -0400

    added some styling changes

--- public/js/main_vm.js
+++ public/js/main_vm.js
@@ -3,22 +3,17 @@ import ChatMessage from "./modules/ChatMessage.js";
 
 const socket = io();
 
-// Sets userNum to current number of other users in room on new join
 function setUserId({sID, count}) {
     vm.socketID = sID;
 
-    vm.userNum = count;
 };
 
 
 //plays sound on user connection
-//adds user to userNum on connection
 //Displays message for new user on connection
 function connectSound(message){
     var connectSound = new Audio("audio/connect.mp3");
     connectSound.play();
-
-    vm.userNum += 1;
 
     if(message !== vm.socketID) {
         socket.emit('connection_message', {
@@ -29,13 +24,10 @@ function connectSound(message){
 }
 
 // Plays sound when on user disconnect
-// Remove user from userNum on disconnect
 // Display message for user disconnect
 function runDisconnectMessage(message) {
     var disconnectSound = new Audio("audio/disconnect.mp3");
     disconnectSound.play();
-
-    vm.userNum -= 1;
 
     socket.emit('connection_message', {
         content: message,
